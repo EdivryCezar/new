946f5c844ff6bffd2ac50dde7dc4667f8afe41f6
EdivryCezar <117872312+EdivryCezar@users.noreply.github.com>
2023-03-16 01:17:39 +0300

    Задание выполнено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat scope =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(scope.getMessage());
+        Cat scope1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(scope1.getMessage());
+
+        System.out.println(bean1==bean);
+        System.out.println(scope1==scope);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String catMessage;
+
+    public String getMessage() {
+        return catMessage;
+    }
+
+    public void setMessage(String catMessage) {
+        this.catMessage = catMessage;
+    }
+}
